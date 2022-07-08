5ece87e7b30d4c21c626b0b4e4c3d789aed7fe83
fernandedfa <deivisondfaa@gmail.com>
2022-07-08 20:36:03 -0300

    configurando status

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { DATABASE } = require('./db');
-
+const pool = require('./db');
 const { PORT } = require('./config');
-
 
 const App = express();
 
@@ -12,63 +10,82 @@ App.use(bodyParser.json());
 App.use(bodyParser.urlencoded({ extended: false }))
 App.use(cors());
 
-
 App.get('/', (req, res) => {
     res.send('hello world')
 })
 
-App.post('/api/register', (req, res) => {
-    const data = {
-        firstName: req.body.firstName,
-        surname: req.body.surname,
-        email: req.body.email,
-        pass: req.body.pass
+App.post('/api/register', async(req, res) => {
+
+    const { firstName, surname, email, pass } = req.body
+
+    const string_DB = {
+        insert: {
+            INSERT_INTO: 'INSERT INTO `db_user` (`name`, `surname`, `email`, `password`, `registered`) VALUES(?, ?, ?, ?, ?)',
+            VALUES_INTO: [firstName, surname, email, pass, new Date()]
+        },
+        querySELECT:  {
+            SELECT: 'SELECT email, password FROM db_user WHERE email=?',
+            VALUES_SELECT: [email]
+        }
+    
     }
 
-    res.status(200).json({
-        status_code: 1,
-        data: req.body
-    })
+    const { INSERT_INTO, VALUES_INTO} = string_DB.insert
+    const { SELECT, VALUES_SELECT } = string_DB.querySELECT
 
-    // ADICIONANDO DADOS NO BANCO DE DADOS
-    DATABASE.insert_db(
-        data.firstName,
-        data.surname,
-        data.email,
-        data.pass
-    )
+
+    await pool.promise().query(SELECT, VALUES_SELECT)
+        .then(([rows, fields]) => {
+
+            if (rows[0] == undefined) {
+                pool.promise().query(INSERT_INTO, VALUES_INTO)
+                    .then(([rows, fields]) => {
+                        res.status(200).send({
+                            status: 'usuário criado com sucesso',
+                            data: req.body
+                        })
+                    })
+                    .catch(console.log)
+            } else if (rows[0].email === email)
+                res.status(401).send({
+                    status: 'usuário já cadastrado',
+                    data: req.body
+                })
+
+        })
+        .catch(console.log)
 })
 
 App.post('/api/login', (req, res) => {
-    const data = {
-        email: req.body.email,
-        pass: req.body.pass
-    }
+    const { email, pass } = req.body
 
-    function success() {
-        if (req.body) {
-            return res.sendStatus(200)
-        }
-    }
+    const querySELECT = 'SELECT email, password FROM db_user WHERE email=? AND password=?'
+    const VALUES = [email, pass]
 
-    function unauthorized() {
-        if (req.body) {
-            return res.sendStatus(401)
-        }
-    }
+    pool.promise().query(querySELECT, VALUES)
 
-    DATABASE.query_DB(
-        data.email,
-        data.pass,
-        success(),
-        unauthorized()
-    )
+        .then(([rows, fields]) => {
 
-    // res.status(200).json({
-    //     status: 'email ou senha errado',
-    //     data: req.body
-    // })
+            if (rows[0] == undefined) {
+                res.status(401).send({
+                    status: 'Usuário ou senha incorreto',
+                    access: 'Negado',
+                    login: req.body
+                    
+                })
+            } else if (rows[0].email === email && rows[0].password === pass) {
+                res.status(200).send({
+                    status: 'Usuário existe',
+                    access: 'Autorizado',
+                    login: req.body
+                    
+
+                })
+            }
+        })
+        .catch(console.log)
 })
+
 
 App.listen(PORT.dev, () => {
     console.log(`Servidor sendo executado na porta: ${PORT.dev}`)
