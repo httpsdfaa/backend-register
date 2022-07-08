72c610be432bf5b3da30e58cb37aa5cb658fec13
fernandedfa <deivisondfaa@gmail.com>
2022-07-08 20:36:20 -0300

    configurando conexao pool

--- db.js
+++ db.js
@@ -1,56 +1,14 @@
 require('dotenv').config();
+
 const mysql = require('mysql2');
-module.exports = {
 
-    DATABASE: {
-        insert_db: function (firstNamePARAM, surnamePARAM, emailPARAM, passPARAM) {
-            const connection = mysql.createConnection({
-                host: process.env.devHOST,
-                user: process.env.devUSER,
-                database: process.env.devDB
-            })
+const pool = mysql.createPool({
+    host: process.env.devHOST,
+    user: process.env.devUSER,
+    database: process.env.devDB,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
 
-            const INSERT = 'INSERT INTO `db_user` (`name`, `surname`, `email`, `password`, `registered`) VALUES(?, ?, ?, ?, ?)'
-            const VALUES = [firstNamePARAM, surnamePARAM, emailPARAM, passPARAM, new Date()]
-
-            connection.query(
-                INSERT,
-                VALUES,
-                function (err, results, fields) {
-                    console.log(results); // results contains rows returned by server
-                    console.log(err)
-                }
-            );
-
-            connection.end();
-        },
-
-        query_DB: function (emailPARAM, passPARAM, userExistPARAM, userNotFoundPARAM) {
-            const connection = mysql.createConnection({
-                host: process.env.devHOST,
-                user: process.env.devUSER,
-                database: process.env.devDB
-            })
-
-            const querySELECT = 'SELECT email, password FROM db_user WHERE email=? AND password=?'
-            const VALUES = [emailPARAM, passPARAM]
-
-            connection.query(
-                querySELECT,
-                VALUES,
-                function (err, results, fields) {
-                    if (results[0]) {
-                        console.log(results[0])
-                        return userExistPARAM
-                    }
-                    else if (!results[0]) {
-                        console.log(results)
-                        return userNotFoundPARAM
-                    }
-                }
-            )
-
-            connection.end()
-        }
-    }
-}
+module.exports = pool;
